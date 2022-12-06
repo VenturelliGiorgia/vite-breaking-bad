9e9794d741f91dddae238c246da8a8aa13235f23
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-12-06 15:55:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import theHeader from './components/theHeader.vue';
+import theFooter from "./components/theFooter.vue";
+export default {
+  components: { theHeader, theFooter}
+};
+</script>
+
+<template>
+<theHeader></theHeader>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+body{
+  background-color:rgb(43 58 71);
+}
+</style>
---
+++ src/components/theHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+<header>
+    <nav>
+        <div class="d-flex align-items-center m-3">
+            <img src="" alt="">
+            <h1 class="text-white">Breaking Bad Api</h1>
+        </div>
+    </nav>
+</header>
+</template>
+
+<style scoped>
+</style>
